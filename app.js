aa2d6b5689b15bd511365ff66e9c5d7d29848d50
And-Bain <find@A5-004029.localdomain>
2018-11-28 17:23:23 -0700

    added style

---
+++ app.js
@@ -0,0 +1,10 @@
+// Count to Dec 25
+
+
+// date for toay
+// the date we want
+// calculate time between today and date we want
+// display on the html
+
+// event listenter, on change, the change of the difference calculation
+
